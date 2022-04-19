895ea8cd3ff399bed8be31d17d421d0cded464fe
Adam Goldsmith <adam.goldsmith75@gmail.com>
2022-04-19 17:23:01 +0100

    Add TF gitlab runners config

---
+++ terraform/gitlab/gitlab runner.tf
@@ -0,0 +1,28 @@
+locals {
+  gitlab_runner_names = {
+    for name, count in var.runner_counts : name => [
+      for i in range(1, count+1) : format("%s-%02d", name, i)
+    ]
+  }
+}
+
+# Create container profile
+resource "lxd_profile" "runner_config" {
+  name = "gitlab_runner_config"
+
+  config = {
+    "limits.cpu"          = 2
+    "limits.memory"       = "2048MB"
+    "user.vendor-data"    = data.template_file.user_data.rendered
+  }
+}
+
+# Create LXD GitLab Runner containers
+resource "lxd_container" "gitlab_runner" {
+  for_each   = toset(local.gitlab_runner_names.runner)
+  name       = each.key
+  # Using a cloud-based image will allow cloud-init configuration (ubuntu images just work)
+  image      = "ubuntu:20.04"
+  ephemeral  = false
+  profiles   = ["default", lxd_profile.runner_config.name]
+}
--- terraform/gitlab/variables.tf
+++ terraform/gitlab/variables.tf
@@ -4,3 +4,10 @@ variable "server_counts" {
     "gitlab"  = 1  # Key name is used for resource prefix
   }
 }
+
+variable "runner_counts" {
+  type    = map(number)
+  default = {
+    "runner"  = 2  # Key name is used for resource prefix
+  }
+}
