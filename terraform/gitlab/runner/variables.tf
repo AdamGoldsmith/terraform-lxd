c1f74f52c329fadf3fa6274e8b2c75867082346e
Adam Goldsmith <adam.goldsmith75@gmail.com>
2022-11-05 16:00:48 +0000

    Increase number of GitLab runners

--- terraform/gitlab/runner/variables.tf
+++ terraform/gitlab/runner/variables.tf
@@ -1,6 +1,6 @@
 variable "instance_counts" {
   type    = map(number)
   default = {
-    "gitlab-runner" = 2  # Key name is used for resource prefix
+    "gitlab-runner" = 4  # Key name is used for resource prefix
   }
 }
